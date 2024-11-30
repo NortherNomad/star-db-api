9a5f3835efc6d7398e63b1374dafa97fc327b405
NortherNomad <kanyshbeisenbek@gmail.com>
2024-11-30 09:04:06 +0500

    small changes

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,6 +1,5 @@
 import React, {Component} from "react"
 import './item-list.css'
-import SwapiService from "../../services/swapi-service"
 import Spinner from "../spinner"
 
 
@@ -9,16 +8,17 @@ export default class ItemList extends Component {
   swapiService = new SwapiService()
   
   state = {
-    peopleList: null
+    itemList: null
   }
 
   componentDidMount() {
-    this.swapiService
-      .getAllPeople()
-      .then((peopleList) => {
-        this.setState({ peopleList })
+    const { getData } = this.props
+
+    getData()
+      .then((itemList) => {
+        this.setState({ itemList })
       })
-  }
+    }
 
   renderItems(arr) {
     return arr.map(({id, name}) => {
@@ -31,19 +31,18 @@ export default class ItemList extends Component {
       )
     })
   }
+  
+  render() {
+    const {itemList} = this.state
 
+    if (!itemList) return <Spinner />
 
-    render() {
-        const {peopleList} = this.state
+    const items = this.renderItems(itemList)
 
-        if (!peopleList) return <Spinner />
-
-        const items = this.renderItems(peopleList)
-
-        return (
-          <ul className="item-list list-group">
-            { items }
-          </ul>
-        )
-    }
+    return (
+      <ul className="item-list list-group">
+        { items }
+      </ul>
+    )
+  }
 }
--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -2,9 +2,13 @@ import React, {Component} from "react";
 import ItemList from "../item-list";
 import PersonDetails from "../person-details";
 import ErrorIndicator from "../error-indicator";
+import SwapiService from "../../services/swapi-service";
 import "./people-page.css"
 
 export default class PeoplePage extends Component {
+
+    swapiService = new SwapiService()
+
     state = {
         selectedPerson: null,
         hasError: false
@@ -28,7 +32,9 @@ export default class PeoplePage extends Component {
         return (
             <div className="row-mb2">
                 <div className="col-md-6">
-                    <ItemList onItemSelected={this.onPersonSelected} />
+                    <ItemList 
+                        onItemSelected={this.onPersonSelected}
+                        getData={this.swapiService.getAllPeople} />
                 </div>
                 <div className="col-md-6">
                     <PersonDetails personId={this.state.selectedPerson} />

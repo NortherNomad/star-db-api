81bd9fc0576c617cda471e6489e5dff42e5a30c7
NortherNomad <kanyshbeisenbek@gmail.com>
2024-12-06 20:15:07 +0500

    hoc composition

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,10 +2,11 @@ import React, {Component} from "react"
 import Header from "../header"
 import RandomPlanet from "../random-planet"
 import ErrorIndicator from "../error-indicator"
-import PeoplePage from "../people-page"
-import SwapiService from "../../services/swapi-service"
-import { PersonList, PlanetList, StarshipList } from "../sw-components"
+import { PersonDetails, PersonList, PlanetList, StarshipList } from "../sw-components"
 import ErrorBoundary from "../error-boundary"
+import DummySwapiService from "../../services/dummy-swapi-service"
+import { SwapiServiceProvider } from "../swapi-service-context"
+import SwapiService from "../../services/swapi-service"
 import "./app.css"
 
 export default class App extends Component {
@@ -29,19 +30,20 @@ export default class App extends Component {
 
         return (
             <ErrorBoundary>
-                <div className="stardb-app">
-                    <Header/>
-                    <RandomPlanet />
-                    <PersonList>
-                        { i => `${i.name} (${i.birthYear})` }
-                    </PersonList>
-                    <StarshipList>
-                        { i => `${i.name} (${i.cargoCapacity})` }
-                    </StarshipList>
-                    <PlanetList>
-                        { i => `${i.name} (${i.population})` }
-                    </PlanetList>
-                </div>
+                <SwapiServiceProvider value={this.swapiService} >
+                    <div className="stardb-app">
+                        <Header/>
+                        <RandomPlanet />
+
+                        <PersonList />
+                        <PersonDetails itemId={2} />
+
+                        <StarshipList />
+
+                        <PlanetList/>
+
+                    </div>
+                </SwapiServiceProvider>
             </ErrorBoundary>
         )
     }
